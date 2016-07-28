85c66387e1ca24484e1a973125dade74eb81cb77
Spencer Davies <spencerhdavies@gmail.com>
2016-07-27 21:51:25 -0600

    Accessors and mutators

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -4,10 +4,35 @@ package com.pluralsight.calcengine;
  * Created by Spencer on 7/27/2016.
  */
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
 
     public void execute() {
         switch (opCode) {
